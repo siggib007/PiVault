e2b9fdbcb3d0860dc9ecefaf97631a5655e374fa
Siggi@Reba <siggib007@users.noreply.github.com>
2022-08-02 21:09:14 +0000

    cleanup

--- CryptoTest.py
+++ CryptoTest.py
@@ -1,4 +1,6 @@
 # pip install pycryptodome
+# encrypt/decrypt functions from https://stackoverflow.com/a/44212550/8549454
+
 import base64
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
@@ -15,34 +17,35 @@ def encrypt(strkey, strData, encode=True):
     oEncrypted = IV + objEncryptor.encrypt(bData)  # store the IV at the beginning and encrypt
     return base64.b64encode(oEncrypted).decode("UTF-8") if encode else oEncrypted
 
-def decrypt(key, source, decode=True):
+
+def decrypt(strkey, strData, decode=True):
     if decode:
-        source = base64.b64decode(source.encode("UTF-8"))
-    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-    IV = source[:AES.block_size]  # extract the IV from the beginning
-    decryptor = AES.new(key, AES.MODE_CBC, IV)
-    data = decryptor.decrypt(source[AES.block_size:])  # decrypt
-    padding = data[-1]  # pick the padding value from the end; 
-    if data[-padding:] != bytes([padding]) * padding:  
+        strData = base64.b64decode(strData.encode("UTF-8"))
+    bKey = bytes(strkey,"UTF-8")
+    hKey = SHA256.new(bKey).digest()
+    IV = strData[:AES.block_size]  # extract the IV from the beginning
+    objEncryptor = AES.new(hKey, AES.MODE_CBC, IV)
+    bClear = objEncryptor.decrypt(strData[AES.block_size:])  # decrypt
+    iPadLen = bClear[-1]  # pick the padding value from the end; 
+    if bClear[-iPadLen:] != bytes([iPadLen]) * iPadLen:  
         raise ValueError("Invalid padding...")
-    bdecr = data[:-padding]  # remove the padding
-    return bdecr.decode("UTF-8")
+    bClear = bClear[:-iPadLen]  # remove the padding
+    return bClear.decode("UTF-8")
 
 
 strPWD = "secret_AES_key_string_to_encrypt/decrypt_with"
-my_password = bytes(strPWD, "UTF-8")
 strTopSecret = "input string to encrypt and decrypt, very top secret stuff!!!"
 
 print("key:  {}".format(strPWD))
 print("data: {}".format(strTopSecret))
 encrypted = encrypt(strPWD, strTopSecret)
 print("\nenc:  {}".format(encrypted))
-decrypted = decrypt(my_password, encrypted)
+decrypted = decrypt(strPWD, encrypted)
 print("dec:  {}".format(decrypted))
 print("\ndata match: {}".format(strTopSecret == decrypted))
 print("\nSecond round....")
 encrypted = encrypt(strPWD, strTopSecret)
 print("\nenc:  {}".format(encrypted))
-decrypted = decrypt(my_password, encrypted)
+decrypted = decrypt(strPWD, encrypted)
 print("dec:  {}".format(decrypted))
 print("\ndata match: {}".format(strTopSecret == decrypted))
